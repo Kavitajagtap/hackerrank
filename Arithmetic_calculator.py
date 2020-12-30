4e12d44808bffa0e26f0bcb2c926e4d0dac608d9
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2020-12-30 15:41:05 +0530

    Update Arithmetic_calculator.py

--- Arithmetic_calculator.py
+++ Arithmetic_calculator.py
@@ -1,3 +1,17 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+def modulo(x,y):
+    return x % y
+
 print("*************Select operation.******************")
 print("1.Add")
 print("2.Subtract")
@@ -14,19 +28,15 @@ while True:
 
     if choice == '1':
        print(num1, "+", num2, "=", add(num1, num2))
-print("*************Select operation.******************")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+    elif choice == '2':
+       print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+       print(num1, "*", num2, "=", multiply(num1, num2))
 
-while True:
+    elif choice == '4':
+           print(num1, "/", num2, "=", divide(num1, num2))
+    elif choice == '5':
+           print(num1, "%", num2, "=", modulo(num1, num2))
 
-    choice = input("Enter choice(1/2/3/4/5): ")
-
-    if choice in ('1', '2', '3', '4','5'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-    if choice == '1':
-       print(num1, "+", num2, "=", add(num1, num2))
+    else:
+       print("Invalid Input")
