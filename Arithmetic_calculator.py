5175b6fc5f0199277d9fc955a5a62575bf9866cf
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2020-12-30 15:37:52 +0530

    Create Arithmetic_calculator.py

---
+++ Arithmetic_calculator.py
@@ -0,0 +1,32 @@
+print("*************Select operation.******************")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4','5'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+       print(num1, "+", num2, "=", add(num1, num2))
+print("*************Select operation.******************")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4','5'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+       print(num1, "+", num2, "=", add(num1, num2))
